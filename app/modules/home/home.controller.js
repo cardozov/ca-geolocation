76816068dd44278457b181923fffe61378ece8e3
Vinicius Goulart Cardozo <vinicius..gcardozo@gmail.com>
2017-07-23 07:09:25 -0300

    [Add] imagens e modais de erro [Fix] Ajustes finos

--- app/modules/home/home.controller.js
+++ app/modules/home/home.controller.js
@@ -2,10 +2,12 @@
 document.addEventListener('DOMContentLoaded',() => {
     InputFileService.createInputTrigger(uploadComponent, uploadBtn, _uploadCallback)
     InputFileService.createInputTrigger(exportComponent, exportBtn, _exportCallback)
+    ipcRenderer.send('load-sender')
+
 })
 
 //-------------- # Module Imports
-const { ipcRenderer } = require('electron')
+const { shell, ipcRenderer } = require('electron')
 const InputFileService = require('../../utils/services/input-file-service.js')
 
 //-------------- # Variables and Properts
@@ -28,6 +30,11 @@ dropContainer.ondragover = () => { return false }
 dropContainer.ondragleave = () => { return false }
 dropContainer.ondragend = () => { return false }
 dropContainer.ondrop = _onDropContainerDrop
+ipcRenderer.on('filter-stop', _onFilterStop)
+ipcRenderer.on('input-short', _onInputShort)
+ipcRenderer.on('output-short', _onOutputShort)
+ipcRenderer.on('test', () => {/*DO SOMETHING*/})
+ipcRenderer.on('process-error', _onProcessError)
 
 //-------------- # Private Functions
 function _uploadCallback(data) {
@@ -51,10 +58,22 @@ function _validateRequirements(){
 
 function _onStartBtnClick(){
     startBtn.setAttribute('disabled', true)
-    ipcRenderer.send('start-process', file, folder)
+    swal({
+        title: "Processando o arquivo.",
+        text: `
+            Só um minuto, por favor...
+            </br></br>
+            <img style="width: 35%; height: auto;" src="../../img/sleeping.png">`,
+        showConfirmButton: false,
+        html: true
+    })
+    setTimeout( () => {
+        ipcRenderer.send('start-process', file, folder)
+    },300)
 }
 
 function _onInfoBtnClick(){
+    swal.close()
     ipcRenderer.send('info-required')
 }
 
@@ -66,8 +85,19 @@ function _onDropContainerDrop(event){
     let pdfList = _getPdfList(data.files)
 
     if(!_isValidFileLists(folderList, pdfList, data.files.length)){
-        //DialogService.showConfirmModal()
-        console.log('errooooou')
+        let text = ""
+        
+        if(folderList.length < 2 && pdfList.length < 2 && data.files.length < 3)
+            text = "Desculpe, não condigo trabalhar com esse tipo de arquivo"
+        else
+            text = "Por favor, escolha somente um PDF e/ou somente uma pasta"
+
+        swal({
+            title: "Ooops!",
+            text: text,
+            confirmButtonText: "Entendi",
+            type: "warning"
+        })
         return false
     }
 
@@ -100,4 +130,46 @@ function _isValidFileLists(folderList = [], pdfList = [], totalLen = -1) {
     let pdfLen = pdfList.length
     let folderLen = folderList.length
     return (folderLen == 1 || folderLen == 0) && (pdfLen == 1 || pdfLen == 0) && (pdfLen + folderLen == totalLen)
+}
+
+function _onFilterStop() {
+    swal({
+        title: "Pronto!",
+        text: `Tudo certo, você já pode se divertir com os novos arquivos!
+                </br></br>
+                Mas tenha cuidado, com grandes <i>arquivos</i> vêm grandes <i>responsabilidades</i>.`,
+        confirmButtonText: "Show",
+        type: "success",
+        html:true
+    }, () => startBtn.removeAttribute('disabled'))
+}
+
+function _onInputShort() {
+    uploadBtn.click()
+}
+
+function _onOutputShort() {
+    exportBtn.click()
+}
+
+function _onProcessError(event, err) {
+    console.log(JSON.parse(err).stack)
+    swal({
+        title: "Ooops!",
+        text: `Tive problemas para entender esse arquivo!
+                </br></br>
+                Por favor, escolha um PDF dentro dos padrões da CETESB, caso ainda não tenha o arquivo, pode enconrtá-lo 
+                <a href="#" onclick="_openExternal()">AQUI</a>
+                </br>
+                Pode encontrar mais dicas e informações clicancdo no botão
+                <a onclick="_onInfoBtnClick()" class="btn-floating z-depth-0 transparent"><i class="material-icons blue-text text-lighten-1">info_outline</i></a>`,
+        confirmButtonText: "Entendi",
+        type: "warning",
+        html:true
+    })
+    startBtn.removeAttribute('disabled')
+}
+
+function _openExternal() {
+    shell.openExternal('http://areascontaminadas.cetesb.sp.gov.br/relacao-de-areas-contaminadas/')
 }
--- app/utils/services/input-file-service.js
+++ app/utils/services/input-file-service.js
@@ -3,19 +3,20 @@ module.exports = class InputFileService {
     constructor(){}
 
     static createInputTrigger(component, trigger, callback){
-        trigger.addEventListener( "keydown", function( event ) {  
+        trigger.addEventListener( "keydown", function( event ) { 
             if ( event.keyCode == 13 || event.keyCode == 32 ) {  
                 component.click();  
             }  
-        })
+        }, false)
 
         trigger.addEventListener( "click", function( event ) {
+            event.preventDefault()
             component.click()
             return false
-        })  
+        }, false)
 
         component.addEventListener( "change", function( event ) {  
             callback(this)
-        })
+        }, false)
     }
 }
